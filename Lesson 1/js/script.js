18bb206f060907cb83dfb38813028cdad922e468
Denys Zhorov <2013dvl@gmail.com>
2019-04-15 21:55:33 +0200

    Update to lesson 2

--- Lesson 1/js/script.js
+++ Lesson 1/js/script.js
@@ -2,8 +2,8 @@
 
 
 
-let money = prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let money = +prompt("Your budget for the month?", ""),
+    time = prompt("Enter the date in the format YYYY-MM-DD", "");
 
 
 
@@ -11,19 +11,83 @@ let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 
 let appData = {
-    money,
+    budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings:false
+    savings: false
 };
 
-let expQuestion1 = prompt("Введите обязательную статью расходов в этом месяце", "");
 
-let expQuestion2 = prompt("Во сколько обойдется?", "");
+///////1 type of cycle
+for (let i = 0; i < 2; i++) {
+
+    let expQuestion1 = prompt("Enter a mandatory expense item for this month.", ""),
+        expQuestion2 = prompt("How much will it cost?", "");
+
+    if ((typeof (expQuestion1)) === 'string' && (typeof (expQuestion1)) != null && (typeof (expQuestion2)) != null &&
+        expQuestion1 != '' && expQuestion2 != '' && expQuestion1.length < 50) {
+        console.log("done");
+        appData.expenses[expQuestion1] = expQuestion2;
+
+    } else {
+        alert("You entered something wrong!")
+        i--;
+
+    }
 
 
-alert(appData.expenses.expQuestion1 = expQuestion2);
-alert(money/30);
+};
+//////// 2 type of cycle
+// let i = 0;
+// while (i < 2) {
 
+//     let expQuestion1 = prompt("Enter a mandatory expense item for this month.", ""),
+//         expQuestion2 = prompt("How much will it cost?", "");
+//     i++;
+//     if ((typeof (expQuestion1)) === 'string' && (typeof (expQuestion1)) != null && (typeof (expQuestion2)) != null &&
+//         expQuestion1 != '' && expQuestion2 != '' && expQuestion1.length < 50) {
+//         console.log("done");
+//         appData.expenses[expQuestion1] = expQuestion2;
+
+//     } else {
+//         alert("You entered something wrong!")
+//         i--;
+
+//     }
+// }
+
+//////////////3 type of cycle
+// let i = 0;
+//  do{
+
+//     let expQuestion1 = prompt("Enter a mandatory expense item for this month.", ""),
+//         expQuestion2 = prompt("How much will it cost?", "");
+//     i++;
+//     if ((typeof (expQuestion1)) === 'string' && (typeof (expQuestion1)) != null && (typeof (expQuestion2)) != null &&
+//         expQuestion1 != '' && expQuestion2 != '' && expQuestion1.length < 50) {
+//         console.log("done");
+//         appData.expenses[expQuestion1] = expQuestion2;
+
+//     } else {
+//         alert("You entered something wrong!")
+//         i--;
+
+//     }
+// }while (i < 2);
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Daily budget : " + appData.moneyPerDay);
+
+if (appData.moneyPerDay <= 100) {
+    console.log("Minimum level of wealth.");
+} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay <= 2000) {
+    console.log("Average level of affluence.");
+} else if (appData.moneyPerDay >= 2000) {
+    console.log("High level of affluence.");
+} else {
+    console.log("Error");
+}
