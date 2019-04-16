6012856d5baa8ed438ac6e904422bb9e8261c307
Denys Zhorov <2013dvl@gmail.com>
2019-04-16 18:55:26 +0200

    Update to lesson 5

--- Lesson 1/js/script.js
+++ Lesson 1/js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-let money, time ;
+let money, time;
+
 function start() {
     money = +prompt("Your budget for the month?", "");
     time = prompt("Enter the date in the format YYYY-MM-DD", "");
@@ -10,82 +11,128 @@ function start() {
     }
 
 }
-
+// start();
 let appData = {
     budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: true
+    savings: true,
+    ///////1 type of cycle
+    chooseExpenses: function () {
+        for (let i = 0; i < 2; i++) {
+
+            let expQuestion1 = prompt("Enter a mandatory expense item for this month.", ""),
+                expQuestion2 = prompt("How much will it cost?", "");
+
+            if ((typeof (expQuestion1)) === 'string' && (typeof (expQuestion1)) != null && (typeof (expQuestion2)) != null &&
+                expQuestion1 != '' && expQuestion2 != '' && expQuestion1.length < 50) {
+                console.log("done");
+                appData.expenses[expQuestion1] = expQuestion2;
+
+            } else {
+                alert("You entered something wrong!");
+                i--;
+
+            }
+        }
+    },
+    detectDayBudget: function () {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+        alert("Daily budget : " + appData.moneyPerDay);
+    },
+    detectLevel: function () {
+        if (appData.moneyPerDay <= 100) {
+            console.log("Minimum level of wealth.");
+        } else if (appData.moneyPerDay >= 100 && appData.moneyPerDay <= 2000) {
+            console.log("Average level of affluence.");
+        } else if (appData.moneyPerDay >= 2000) {
+            console.log("High level of affluence.");
+        } else {
+            console.log("Error");
+        }
+    },
+    checkSavings: function () {
+        if (appData.savings == true) {
+            let save = +prompt("What is the amount of savings?", ""),
+                precent = +prompt("What percentage?", "");
+
+            appData.monthIncome = save / 100 / 12 * precent;
+            alert("Monthly income from your deposit");
+        }
+    },
+    chooseOptExpenses: function () {
+        for (let i = 1; i < 4; i++) {
+            let a = +prompt("An optional expense?", "");
+            appData.optionalExpenses[i] = a;
+            console.log(appData.optionalExpenses[i]);
+        }
+    },
+    chooseIncome: function () {
+
+        // let items = prompt("What brings additional income?(separated by commas)", '');
+        let arr = ["efe", "ef","2"];
+        console.log(typeof (items));
+        // appData.income = +items.split(",");
+        console.log(typeof (appData.income));
+function comparisonsArr(){
+    let result = arr.filter(function (sum) {
+            return typeof (sum) === "string";
+        });
+        alert(result);
+        console.log(result);
+
+        if (arr.length == result.length) {
+            return true;
+        } else {
+            return false;
+        }
+}
+        
+alert(comparisonsArr());
+
+
+        // if (comparisonsArr() || items == '' || (typeof (items)) == null) {
+
+        //     alert("You entered something wrong!");
+
+        // } else {
+
+        //     appData.income.push(prompt("Maybe something else?", ""));
+        //     appData.income.sort();
+
+        // }
+
+
+
+
+
+        // let items = prompt("What brings additional income?(separated by commas)", '');
+
+        // console.log(typeof (items));
+        // console.log(appData.income.length);
+        // if ((typeof (items)) != 'string' || items == '' || (typeof (items)) == null) {
+
+        //     alert("You entered something wrong!");
+
+        // } else {
+        //     appData.income = items.split(",");
+        //     appData.income.push(prompt("Maybe something else?", ""));
+        //     appData.income.sort();
+
+        // }
+    }
 };
 
-///////1 type of cycle
-function chooseExpenses() {
-    for (let i = 0; i < 2; i++) {
 
-        let expQuestion1 = prompt("Enter a mandatory expense item for this month.", ""),
-            expQuestion2 = prompt("How much will it cost?", "");
-
-        if ((typeof (expQuestion1)) === 'string' && (typeof (expQuestion1)) != null && (typeof (expQuestion2)) != null &&
-            expQuestion1 != '' && expQuestion2 != '' && expQuestion1.length < 50) {
-            console.log("done");
-            appData.expenses[expQuestion1] = expQuestion2;
-
-        } else {
-            alert("You entered something wrong!")
-            i--;
-
-        }
-
-
-    }
-}
-
-function detectDayBudget() {
-    appData.moneyPerDay = (appData.budget / 30).toFixed();
-
-    alert("Daily budget : " + appData.moneyPerDay);
-}
-
-function detectLevel() {
-    if (appData.moneyPerDay <= 100) {
-        console.log("Minimum level of wealth.");
-    } else if (appData.moneyPerDay >= 100 && appData.moneyPerDay <= 2000) {
-        console.log("Average level of affluence.");
-    } else if (appData.moneyPerDay >= 2000) {
-        console.log("High level of affluence.");
-    } else {
-        console.log("Error");
-    }
-
-}
-
-function checkSavings() {
-    if (appData.savings == true) {
-        let save = +prompt("What is the amount of savings?", ""),
-            precent = +prompt("What percentage?", "")
-
-        appData.monthIncome = save / 100 / 12 * precent;
-        alert("Monthly income from your deposit")
-    }
-}
-
-
-function chooseOptExpenses() {
-    for(let i = 1; i < 4; i++){
-        let a = +prompt("An optional expense?", "");
-        appData.optionalExpenses[i] = a;
-    console.log(appData.optionalExpenses[i]);
-    }
-    
-}
-// start();
+appData.chooseIncome();
 // chooseExpenses();
 // detectDayBudget();
 // detectLevel();
 // checkSavings();
-chooseOptExpenses();
+// chooseOptExpenses();
 
 //////// 2 type of cycle
 // let i = 0;
--- Lesson 1/js/tempCodeRunnerFile.js
+++ Lesson 1/js/tempCodeRunnerFile.js
@@ -1,12 +1,14 @@
-var str = "Ослик Иа-Иа посмотрел на виадук"; // ищем в этой строке
-var target = "Иа"; // цель поиска
 
-var pos = 0;
-while (true) {
-  var foundPos = str.indexOf(target, pos);
-//   consol.log(foundPos);
-  if (foundPos == -1) break;
+let a = {
+    h:400,
+    c:200
+};
 
-  alert( foundPos ); // нашли на этой позиции
-  pos = foundPos + 1; // продолжить поиск со следующей
-}
+let f = {
+    h:100
+};
+
+f.__proto__ = a;
+
+console.log(f);
+console.log(f.c);
