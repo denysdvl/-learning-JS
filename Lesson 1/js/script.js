c002f87deaa556a5dd48b789beeb9563149c5b80
Denys Zhorov <2013dvl@gmail.com>
2019-04-16 12:02:15 +0200

    Update to lesson 4,

--- Lesson 1/js/script.js
+++ Lesson 1/js/script.js
@@ -1,14 +1,15 @@
 'use strict';
 
-
-
-let money = +prompt("Your budget for the month?", ""),
+let money, time ;
+function start() {
+    money = +prompt("Your budget for the month?", "");
     time = prompt("Enter the date in the format YYYY-MM-DD", "");
 
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Your budget for the month?", "");
+    }
 
-
-
-
+}
 
 let appData = {
     budget: money,
@@ -16,29 +17,76 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
-
 
 ///////1 type of cycle
-for (let i = 0; i < 2; i++) {
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
 
-    let expQuestion1 = prompt("Enter a mandatory expense item for this month.", ""),
-        expQuestion2 = prompt("How much will it cost?", "");
+        let expQuestion1 = prompt("Enter a mandatory expense item for this month.", ""),
+            expQuestion2 = prompt("How much will it cost?", "");
 
-    if ((typeof (expQuestion1)) === 'string' && (typeof (expQuestion1)) != null && (typeof (expQuestion2)) != null &&
-        expQuestion1 != '' && expQuestion2 != '' && expQuestion1.length < 50) {
-        console.log("done");
-        appData.expenses[expQuestion1] = expQuestion2;
+        if ((typeof (expQuestion1)) === 'string' && (typeof (expQuestion1)) != null && (typeof (expQuestion2)) != null &&
+            expQuestion1 != '' && expQuestion2 != '' && expQuestion1.length < 50) {
+            console.log("done");
+            appData.expenses[expQuestion1] = expQuestion2;
 
-    } else {
-        alert("You entered something wrong!")
-        i--;
+        } else {
+            alert("You entered something wrong!")
+            i--;
+
+        }
+
 
     }
+}
+
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+    alert("Daily budget : " + appData.moneyPerDay);
+}
+
+function detectLevel() {
+    if (appData.moneyPerDay <= 100) {
+        console.log("Minimum level of wealth.");
+    } else if (appData.moneyPerDay >= 100 && appData.moneyPerDay <= 2000) {
+        console.log("Average level of affluence.");
+    } else if (appData.moneyPerDay >= 2000) {
+        console.log("High level of affluence.");
+    } else {
+        console.log("Error");
+    }
+
+}
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("What is the amount of savings?", ""),
+            precent = +prompt("What percentage?", "")
+
+        appData.monthIncome = save / 100 / 12 * precent;
+        alert("Monthly income from your deposit")
+    }
+}
 
 
-};
+function chooseOptExpenses() {
+    for(let i = 1; i < 4; i++){
+        let a = +prompt("An optional expense?", "");
+        appData.optionalExpenses[i] = a;
+    console.log(appData.optionalExpenses[i]);
+    }
+    
+}
+// start();
+// chooseExpenses();
+// detectDayBudget();
+// detectLevel();
+// checkSavings();
+chooseOptExpenses();
+
 //////// 2 type of cycle
 // let i = 0;
 // while (i < 2) {
@@ -76,18 +124,3 @@ for (let i = 0; i < 2; i++) {
 
 //     }
 // }while (i < 2);
-
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Daily budget : " + appData.moneyPerDay);
-
-if (appData.moneyPerDay <= 100) {
-    console.log("Minimum level of wealth.");
-} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay <= 2000) {
-    console.log("Average level of affluence.");
-} else if (appData.moneyPerDay >= 2000) {
-    console.log("High level of affluence.");
-} else {
-    console.log("Error");
-}
--- Lesson 1/js/tempCodeRunnerFile.js
+++ Lesson 1/js/tempCodeRunnerFile.js
@@ -1,8 +1,12 @@
-let persone = {
-    name: 'Jone',
-    age: 25,
-    isMarred: false
-};
+var str = "Ослик Иа-Иа посмотрел на виадук"; // ищем в этой строке
+var target = "Иа"; // цель поиска
 
+var pos = 0;
+while (true) {
+  var foundPos = str.indexOf(target, pos);
+//   consol.log(foundPos);
+  if (foundPos == -1) break;
 
-console.log(persone["age"]);
+  alert( foundPos ); // нашли на этой позиции
+  pos = foundPos + 1; // продолжить поиск со следующей
+}
